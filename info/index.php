b6b56dc6c3e637d4891303d7d2342d476b8ececf
RubensGabrielVelazquezFonseca <55161653+RubensGabrielVelazquezFonseca@users.noreply.github.com>
2022-02-11 10:56:25 -0300

    Update index.php

--- info/index.php
+++ info/index.php
@@ -5,12 +5,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
-    <title>Informações</title>
+    <title>Hobbies</title>
 </head>
 <body>
     <main class="container">
         <header class="container__header">
-            <h1>Cadastro de Informações</h1>
+            <h1>Cadastro de suas informações</h1>
             <p>Deixe-nos entender quais são seus gostos e interesses</p>
         </header>
         <form action="user.php" class="form-container" method="POST">
@@ -44,7 +44,27 @@
                     <label for="phone-number">
                         <strong>N° de celular: </strong>
                     </label>
-                </div>
+ </div>
+
+ <div class="form-container__input-container">
+                    <input type="text" id="cpf_number" name="cpf-number" required />
+                
+                    <label for="cpf-number">
+                        <strong>CPF: </strong>
+                    </label>
+ </div>
+
+
+ <div class="form-container__input-container">
+                    <input type="text" id="rg_number" name="rg-number" required />
+                
+                    <label for="rd-number">
+                        <strong>RG: </strong>
+                    </label>
+ </div>
+
+
+
                 <div class="form-container__input-container">
                     <input type="text" id="address" name="address" required />
                     <label for="address">
@@ -53,119 +73,152 @@
                 </div>
             </fieldset>
             <fieldset class="form-container__fieldset">
-                <legend>Hobbies: </legend>
-               <div class="form-container__checkbox-container">
-                    <p>Escolha seus Hobbie: </p>
+                <legend>Seus Hobbis: </legend>
+                <div class="form-container__checkbox-container">
+                    <p>Diga seus Hobbies: </p>
                     <section>
-                    <label for="Anime" class="form-container__input-container__checkbox">
-                            <input type="checkbox" name="programming_ss[]" value="Anime" id="Anime">
-                            <span>Anime</span>
-                        </label>
                         <label for="Manga" class="form-container__input-container__checkbox">
-                            <input type="checkbox" name="programming_ss[]" value="Manga" id="Manga">
+                            <input type="checkbox" name="programming_hobbis[]" value="Manga" id="Manga">
                             <span>Manga</span>
                         </label>
-                        <label for="Light Novel" class="form-container__input-container__checkbox">
-                            <input type="checkbox" name="programming_ss[]" value="Light Novel" id="Light Novel">
-                            <span>Light Novel</span>
+                        <label for="Anime" class="form-container__input-container__checkbox">
+                            <input type="checkbox" name="programming_hobbis[]" value="Anime" id="Anime">
+                            <span>Anime</span>
                         </label>
-                        <label for="Fanfic" class="form-container__input-container__checkbox">
-                            <input type="checkbox" name="programming_ss[]" value="Fanfic" id="Fanfic">
-                            <span>Fanfic</span>
+                        <label for="Ligh Novel" class="form-container__input-container__checkbox">
+                            <input type="checkbox" name="programming_hobbis[]" value="Ligh Novel" id="Ligh Novel">
+                            <span>Ligh Novel</span>
                         </label>
-                        <label for="Jogar Video Game" class="form-container__input-container__checkbox">
-                            <input type="checkbox" name="programming_ss[]" value="Jogar Video Game" id="Jogar Video Game">
-                            <span>Jogar Video Game</span>
+                        <label for="Livros" class="form-container__input-container__checkbox">
+                            <input type="checkbox" name="programming_hobbis[]" value="Livros" id="Livros">
+                            <span>Livros</span>
+                        </label>
+                        <label for="Fanfics" class="form-container__input-container__checkbox">
+                            <input type="checkbox" name="programming_hobbis[]" value="Fanfics" id="Fanfics">
+                            <span>Fanfics</span>
                         </label>
                     </section>
                 </div>
                 <div class="form-container__input-container-wrapper">
                     <div class="form-container__input-container" data-select>
-                        <input name="programming_oo" id="programming_oo" type="text" required class="multiple-select-wrapper">
-                        <label for="programming_oo">
-                            <strong>Preferencia em tema: </strong>
+                        <input name="programming_genero" id="programming_genero" type="text" required class="multiple-select-wrapper">
+                        <label for="programming_genero">
+                            <strong>Generos: </strong>
                         </label>
                     </div>
+                    <select class="multiple-select" multiple>
+                            <option value="Romance">Romance</option>
+                            <option value="Aventura">Aventura</option>
+                            <option value="Isekai">Isekai</option>
+                            <option value="Hentai">Hentai</option>
+                            <option value="Comedia">Comedia</option>
+                        </select>
+                </div>
 
-                    <fieldset class="form-container__fieldset">
-                <legend>Esporte: </legend>
-               <div class="form-container__checkbox-container">
-                    <p>Escolha seu Esporte: </p>
+                <fieldset class="form-container__fieldset">
+                <legend>Esports: </legend>
+                <div class="form-container__checkbox-container">
+                    <p>Diga seus Esportes que vc gosta: </p>
                     <section>
                         <label for="Futebol" class="form-container__input-container__checkbox">
-                            <input type="checkbox" name="programming_ss[]" value="Futebol" id="Futebol">
+                            <input type="checkbox" name="programming_sport[]" value="Futebol" id="Futebol">
                             <span>Futebol</span>
                         </label>
                         <label for="Basquete" class="form-container__input-container__checkbox">
-                            <input type="checkbox" name="programming_ss[]" value="Basquete" id="Basquete">
+                            <input type="checkbox" name="programming_sport[]" value="Basquete" id="Basquete">
                             <span>Basquete</span>
                         </label>
                         <label for="Handebol" class="form-container__input-container__checkbox">
-                            <input type="checkbox" name="programming_ss[]" value="Handebol" id="Handebol">
+                            <input type="checkbox" name="programming_sport[]" value="Handebol" id="Handebol">
                             <span>Handebol</span>
                         </label>
-                        <label for="Tenis" class="form-container__input-container__checkbox">
-                            <input type="checkbox" name="programming_ss[]" value="Tenis" id="Tenis">
-                            <span>Tenis</span>
-                        </label>
                         <label for="Ping Pong" class="form-container__input-container__checkbox">
-                            <input type="checkbox" name="programming_ss[]" value="Ping Pong" id="Ping Pong">
+                            <input type="checkbox" name="programming_sport[]" value="Ping Pong" id="Ping Pong">
                             <span>Ping Pong</span>
+                        </label>
+                        <label for="Tenis" class="form-container__input-container__checkbox">
+                            <input type="checkbox" name="programming_sport[]" value="Tenis"" id="Tenis">
+                            <span>Tenis"</span>
                         </label>
                     </section>
                 </div>
                 <div class="form-container__input-container-wrapper">
                     <div class="form-container__input-container" data-select>
-                        <input name="programming_oo" id="programming_oo" type="text" required class="multiple-select-wrapper">
-                        <label for="programming_oo">
-                            <strong>Preferencia modalide: </strong>
+                        <input name="programming_time" id="programming_time" type="text" required class="multiple-select-wrapper">
+                        <label for="programming_time">
+                            <strong>Times: </strong>
                         </label>
                     </div>
-                   
-                    <fieldset class="form-container__fieldset">
-                <legend>Carros: </legend>
-               <div class="form-container__checkbox-container">
-                    <p>Escolha seu Caro: </p>
+                    <select class="multiple-select" multiple>
+                            <option value="São Paulo"O T>São Paulo</option>
+                            <option value="Santos">Santos</option>
+                            <option value="Palmeiras">Palmeiras</option>
+                            <option value="Flamengo">Flamengo</option>
+                            <option value="Curintia">Curintia</option>
+                        </select>
+                </div>
+
+
+
+                <fieldset class="form-container__fieldset">
+                <legend>Esports: </legend>
+                <div class="form-container__checkbox-container">
+                    <p>Diga seu carro e marca favoritos: </p>
                     <section>
                         <label for="Fusca" class="form-container__input-container__checkbox">
-                            <input type="checkbox" name="programming_ss[]" value="Fusca" id="Fusca">
+                            <input type="checkbox" name="programming_carro[]" value="Fusca" id="Fusca">
                             <span>Fusca</span>
                         </label>
                         <label for="Picape" class="form-container__input-container__checkbox">
-                            <input type="checkbox" name="programming_ss[]" value="Picape" id="Picape">
+                            <input type="checkbox" name="programming_carro[]" value="Picape" id="Picape">
                             <span>Picape</span>
                         </label>
                         <label for="Hatch" class="form-container__input-container__checkbox">
-                            <input type="checkbox" name="programming_ss[]" value="Hatch" id="Hatch">
+                            <input type="checkbox" name="programming_carro[]" value="Hatch" id="Hatch">
                             <span>Hatch</span>
                         </label>
                         <label for="Cupê" class="form-container__input-container__checkbox">
-                            <input type="checkbox" name="programming_ss[]" value="Cupê" id="Cupê">
+                            <input type="checkbox" name="programming_carro[]" value="Cupê" id="Cupê">
                             <span>Cupê</span>
                         </label>
                         <label for="Jipe" class="form-container__input-container__checkbox">
-                            <input type="checkbox" name="programming_ss[]" value="Jipe" id="Jipe">
-                            <span>Jipe</span>
+                            <input type="checkbox" name="programming_carro[]" value="Jipe"" id="Jipe">
+                            <span>Jipe"</span>
                         </label>
                     </section>
                 </div>
                 <div class="form-container__input-container-wrapper">
                     <div class="form-container__input-container" data-select>
-                        <input name="programming_oo" id="programming_oo" type="text" required class="multiple-select-wrapper">
-                        <label for="programming_oo">
-                            <strong>Marca: </strong>
+                        <input name="programming_marca" id="programming_Marca" type="text" required class="multiple-select-wrapper">
+                        <label for="programming_marca">
+                            <strong>Marcas: </strong>
                         </label>
                     </div>
-
-
+                    <select class="multiple-select" multiple>
+                            <option value="Fiat"O T>Fiat</option>
+                            <option value="Volk">Volk</option>
+                            <option value="Ford">Ford</option>
+                            <option value="Nissan">Nissan</option>
+                            <option value="Toyota">Toyota</option>
+                        </select>
                 </div>
+
+
+
+
+
+
+
+
+
+
+
             </fieldset>
             <fieldset class="form-container__fieldset">
                 <button class="form-container__send-button">Enviar dados</button>
             </fieldset>
         </form>
-        <footer class="container__footer">
-        </footer>
+       
     </main>
     <script src="./scripts/index.js"></script>
 </body>
