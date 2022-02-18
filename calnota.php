92c11f8f1173af5eed66c21998dcdc656c392da4
RubensGabrielVelazquezFonseca <55161653+RubensGabrielVelazquezFonseca@users.noreply.github.com>
2022-02-18 17:01:30 -0300

    Create calnota.php

---
+++ calnota.php
@@ -0,0 +1,29 @@
+<html>
+ <head>
+  <title>Media Aluno</title>
+ </head>
+ <body>
+ <form action="" method="get">
+ Nota 1:  <input type="text" name="nota1" /><br />
+  Nota 2:  <input type="text" name="nota2" /><br />
+  Nota 3:  <input type="text" name="nota3" /><br />
+  Nota 4:  <input type="text" name="nota4" /><br />
+  <input type="submit" name="submit" value="Testar" />
+ </form> 
+ <?php 
+  $nota1 = $_GET['nota1'];
+  $nota2 = $_GET['nota2'];
+  $nota3 = $_GET['nota3'];
+  $nota4 = $_GET['nota4'];
+  $media = ($nota1 + $nota2 + $nota3 + $nota4)/4;
+
+  echo "Média: $media <br />";
+  if($media<7)
+   echo "Reprovado";
+  elseif($media==10)
+   echo "Aprovado com Honra ao mérito";
+  else
+   echo "Aprovado";
+ ?>
+ </body>
+</html>
